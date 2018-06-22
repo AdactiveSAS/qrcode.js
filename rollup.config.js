32ee048a8568817a8652a21f4b791e57a6f30a68
Stanley <Stanley92@hotmail.com>
2018-06-22 12:08:43 +0800

    Modified rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,70 @@
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+import uglifyEs from 'rollup-plugin-uglify-es';
 import pkg from './package.json';
 
-export default [
-    {
-        entry: 'src/qrcode.js',
-        dest: pkg.browser,
+const configBrowser = {
+    input: 'src/qrcode.js',
+    output: {
+        name: 'qrcode',
+        file: pkg.browser,
         format: 'umd',
-        moduleName: 'qrcode',
-        plugins: [
-            babel({
-                exclude: ['node_modules/**']
-            })
-        ]
+        globals: {
+            'qrcode-generator': 'VendorQrCode'
+        },
+        sourcemap: true
     },
-    {
-        entry: 'src/qrcode.js',
-        external: ['qrcode-generator'],
-        targets: [
-            { dest: pkg.main, format: 'cjs' },
-            { dest: pkg.module, format: 'es' }
-        ],
-        plugins: [
-            babel({
-                exclude: ['node_modules/**']
-            })
-        ]
-    }
+    external: ['qrcode-generator'],
+    plugins: [
+        babel({ exclude: ['node_modules/**'] }),
+        resolve(),
+        uglify(),
+        commonjs()
+    ]
+};
+
+const configNode = {
+    input: 'src/qrcode.js',
+    output: {
+        name: 'qrcode',
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+    },
+
+    external: ['qrcode-generator'],
+    plugins: [
+        babel({ exclude: ['node_modules/**'] }),
+        resolve(),
+        uglify(),
+        commonjs()
+    ]
+};
+
+const configModule = {
+    input: 'src/qrcode.js',
+    output: {
+        name: 'qrcode',
+        file: pkg.module,
+        format: 'es',
+        globals: {
+            'qrcode-generator': 'VendorQrCode'
+        },
+        sourcemap: true
+    },
+    external: ['qrcode-generator'],
+    plugins: [
+        babel({ exclude: ['node_modules/**'] }),
+        resolve(),
+        uglifyEs(),
+        commonjs()
+    ]
+};
+
+export default [
+    configBrowser,
+    configModule,
+    configNode
 ];
